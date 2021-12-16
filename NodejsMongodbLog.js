3be360c2c516fa71df25087137c90d1eaf0bac67
wt627 <2214166877@qq.com>
2021-12-15 16:14:01 -0800

    resultsuccess

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app=express()
+const ejs = require("ejs")
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://172.21.2.236:27017/190110910627');
 const schema = {
@@ -14,9 +15,12 @@ const mydata = mongoose.model('cat1s', schema);
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    //res.send(req.query)
     console.log(req.query)
     const kitty = new mydata({ name: req.query.first,health:req.query.second });
     kitty.save()
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+        res.send(str)
+    })
 })
 app.listen(10627)
